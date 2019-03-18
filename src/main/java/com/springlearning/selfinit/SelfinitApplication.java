c189aafbc5265586f9e7dabbf9aec388ae5bb5b0
someone <you@example.com>
2019-03-18 11:04:39 -0700

    test for best alternative

---
+++ src/main/java/com/springlearning/selfinit/SelfinitApplication.java
@@ -0,0 +1,13 @@
+package com.springlearning.selfinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfinitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springlearning/selfinit/SelfinitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springlearning.selfinit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SelfinitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
